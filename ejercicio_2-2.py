eb886d424981e4010b829f09cc11e1add22f35fc
BastianHuenchullan <ba.huenchullan@duocuc.cl>
2025-06-10 15:13:04 -0400

    Ejercicio_2
    Lista de invitados y organización de mesas.

---
+++ ejercicio_2-2.py
@@ -0,0 +1,77 @@
+invitados_menores = {}
+invitados_adolescentes = {}
+invitados_adultos = {}
+
+# Cantidad de personas a registrar.
+
+while True:
+    try:
+        cantidad = int(input("¿Cuántas personas quieres registrar? "))
+
+        if cantidad < 0:
+            print("Debes ingresar al menos 1 usuario.")
+            continue
+        else:
+            break
+    except ValueError:
+        print("Valor incorrecto. Debes ingresar un número.")
+        continue
+
+# Pregunta el nombre y edad de cada persona y finalmente lo agrega a los diccionarios según su edad.
+
+print()
+
+for i in range(cantidad):
+    while True:
+        nombre = input(f"Nombre del invitado N°{i+1}: ").upper()
+
+        if nombre in invitados_menores or nombre in invitados_adolescentes or nombre in invitados_adultos:
+            print("Ese nombre ya está registrado en el sistema. Prueba con otro.")
+            continue
+        else:
+            break
+
+    while True:
+        try:
+            edad = int(input("- ¿Cuál es su edad? "))
+            if edad < 0:
+                print("Debes ingresar una edad mayor a 0.")
+                continue
+            else:
+                break
+        except ValueError:
+            print("Valor incorrecto. Debes ingresar un número.")
+            continue
+    
+    if edad < 12:
+        print("Lo sentimos, pero la entrada no está permitida para esta persona, ya que sólo aceptamos a mayores de 12 años.\n")
+    elif 12 <= edad <= 17:
+        print(f"{nombre} ha sido agregado a la Mesa 1.\n")
+        invitados_menores.update({nombre: edad})
+    elif 18 <= edad <= 29:
+        print(f"{nombre} ha sido agregado a la Mesa 2.\n")
+        invitados_adolescentes.update({nombre: edad})
+    elif edad >= 30:
+        print(f"{nombre} ha sido agregado a la Mesa 3.\n")
+        invitados_adultos.update({nombre: edad})
+
+
+# Imprimir mesas con lista completa de invitados.
+
+if len(invitados_menores) == 0:
+    print("Mesa 1: No hay ningún invitado.")
+else:
+    lista_menores = [f"{nombre} ({edad} años)" for nombre, edad in invitados_menores.items()]
+    print("Mesa 1: " + ", ".join(lista_menores))
+
+if len(invitados_adolescentes) == 0:
+    print("Mesa 2: No hay ningún invitado.")
+else:
+    lista_adolescentes = [f"{nombre} ({edad} años)" for nombre, edad in invitados_adolescentes.items()]
+    print("Mesa 2: " + ", ".join(lista_adolescentes))
+
+if len(invitados_adultos) == 0:
+    print("Mesa 3: No hay ningún invitado.")
+else:
+    lista_adultos = [f"{nombre} ({edad} años)" for nombre, edad in invitados_adultos.items()]
+    print("Mesa 3: " + ", ".join(lista_adultos))
