d4c328026b723f1c4052b6e5f1230140b4268dde
BastianHuenchullan <ba.huenchullan@duocuc.cl>
2025-06-17 15:38:19 -0400

    Venta de pasajes de bus (Parte 2: Avanzado)

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -3,6 +3,7 @@
 
 
 asientos_disponibles = [1, 2, 3, 4, 5]
+asientos_registrados = {}
 
 while True:
     try:
@@ -11,7 +12,8 @@ while True:
 
 1.- Ver asientos disponibles
 2.- Comprar pasaje
-3.- Salir
+3.- Ver pasajeros registrados
+4.- Salir
 
 Seleccione una opción: """))
     except ValueError:
@@ -19,8 +21,18 @@ Seleccione una opción: """))
         continue
 
     if opcion == 1:
-        print(f"- Asientos disponibles: {asientos_disponibles}")
+        print("- Asientos disponibles: " + ", ".join(str(asiento) for asiento in asientos_disponibles))
+
     elif opcion == 2:
+        
+        rut = input("Ingrese su RUT: ").replace(" ", "").replace("-", "").replace(".", "").upper()
+
+        if (rut[-1].isdigit() or rut[-1] == "K") and rut[:-1].isdigit():
+            rut_formateado = rut[:-1] + "-" + rut[-1]
+        else:
+            print("- RUT Inválido.")
+            continue
+
         try:
             opcion_compra = int(input("Ingrese número de asiento a comprar: "))
         except ValueError:
@@ -28,17 +40,26 @@ Seleccione una opción: """))
             continue
 
         if opcion_compra in asientos_disponibles:
-            print(f"- Asiento {opcion_compra} comprado con éxito.")
+            print(f"- Asiento {opcion_compra} comprado con éxito para el RUT {rut_formateado}.")
             asientos_disponibles.remove(opcion_compra)
+            asientos_registrados.update({rut_formateado: opcion_compra})
         else:
             if opcion_compra in (1, 2, 3, 4, 5):
                 print("- Lo sentimos, ese asiento ya no está disponible. Prueba con otro.")
             else:
-                print("- Elige un asiento valido.")
+                print("- Elige un asiento válido.")
+
     elif opcion == 3:
+        print("--- Lista de pasajeros ---")
+        if asientos_registrados:
+            for rut, asiento in asientos_registrados.items():
+                print(f"- RUT: {rut}, Asiento: {asiento}")
+        else:
+            print("- No hay ningún pasajero ni asientos reservados aún.")
+
+    elif opcion == 4:
         print("- ¡Hasta luego! Vuelva pronto.")
         break
-    else:
-        print("- Opción incorrecta. Por favor, seleccione una opción entre 1-3.")
 
-    continue
+    else:
+        print("- Opción incorrecta. Por favor, seleccione una opción entre 1-4.")
